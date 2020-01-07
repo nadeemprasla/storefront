7972a0146f769599b243a0962b4f3664911ad9ee
Nadeem Prasla <nadeemprasla@yahoo.com>
2020-01-07 10:43:35 -0600

    deploying to heroku

---
+++ client/build/precache-manifest.3224b29e292921a99ac453754482e0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4d15797528138d1026b8a747a60bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d0fc2b853801ff7260",
+    "url": "/static/css/main.0dd3c909.chunk.css"
+  },
+  {
+    "revision": "9133e9fe389998e1d1e5",
+    "url": "/static/js/2.7eca4f1c.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/static/js/2.7eca4f1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5d0fc2b853801ff7260",
+    "url": "/static/js/main.e7067406.chunk.js"
+  },
+  {
+    "revision": "de50d0be950889458790",
+    "url": "/static/js/runtime-main.725fb81a.js"
+  }
+]);

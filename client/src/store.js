ce478289462ee95ef4001386df6c1c4b61ca2a14
Nadeem Prasla <nadeemprasla@yahoo.com>
2020-01-10 11:00:35 -0600

    redux fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const initialState = {};
 
@@ -10,7 +9,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  compose(applyMiddleware(...middleware))
 );
 
 export default store;
